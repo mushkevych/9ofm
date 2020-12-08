d8dd04fce8f851bb50162fe13a814da807d2956c
Bohdan Mushkevych <mushkevych@gmail.com>
2020-12-07 22:51:33 -0800

    Fixing plan9/linux cross-compilation for GetXid

--- commander/model/file_details_plan9.go
+++ commander/model/file_details_plan9.go
@@ -3,14 +3,15 @@
 package model
 
 import (
-	sysPlan9 "golang.org/x/sys/plan9"
+	//sysPlan9 "golang.org/x/sys/plan9"
+	"syscall"
 	"os"
 )
 
 func GetXid(info os.FileInfo) (string, string) {
 	UID := "N/A"
 	GID := "N/A"
-	if stat, ok := info.Sys().(*sysPlan9.Dir); ok {
+	if stat, ok := info.Sys().(*syscall.Dir); ok {
 		UID = stat.Uid
 		GID = stat.Gid
 	}
